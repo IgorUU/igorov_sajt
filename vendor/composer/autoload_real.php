8bbba4dcd677697332076b45336c70e8366d2e19
Igor <igor@studiopresent.com>
2023-04-16 21:51:43 +0200

    Require drush 11

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -37,25 +37,18 @@ class ComposerAutoloaderInit5371e219b2e10d827412380a38f1b0b0
 
         $loader->register(true);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInit5371e219b2e10d827412380a38f1b0b0::$files;
-        foreach ($includeFiles as $fileIdentifier => $file) {
-            composerRequire5371e219b2e10d827412380a38f1b0b0($fileIdentifier, $file);
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit5371e219b2e10d827412380a38f1b0b0::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
-    }
-}
-
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
-function composerRequire5371e219b2e10d827412380a38f1b0b0($fileIdentifier, $file)
-{
-    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-        require $file;
     }
 }
