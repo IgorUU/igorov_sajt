e874e8f06bb8b598fb6210ff75117d6bf0487df1
Igor <igor@studiopresent.com>
2023-05-10 22:15:17 +0200

    Create custom theme

---
+++ web/themes/ravnoteza/js/ravnoteza.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Ravnoteza behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ravnoteza = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
