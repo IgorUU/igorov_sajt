c119868e1d271d6d2f29480db785ecb91e4a8efc
Igor <igor@studiopresent.com>
2023-05-10 22:00:34 +0200

    Update Drupal core, 10.0.9

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -293,8 +293,8 @@ class ComposerStaticInit5371e219b2e10d827412380a38f1b0b0
         ),
         'Psr\\Http\\Message\\' => 
         array (
-            0 => __DIR__ . '/..' . '/psr/http-factory/src',
-            1 => __DIR__ . '/..' . '/psr/http-message/src',
+            0 => __DIR__ . '/..' . '/psr/http-message/src',
+            1 => __DIR__ . '/..' . '/psr/http-factory/src',
         ),
         'Psr\\Http\\Client\\' => 
         array (
